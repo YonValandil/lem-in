2eacc35b5c547b49887a78bbe97e9b60ae355764
Jimmy JOURNE <jjourne@e1r10p7.42.fr>
2018-05-01 18:10:24 +0200

    comment

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jjourne <jjourne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/01 12:02:07 by jjourne           #+#    #+#             */
-/*   Updated: 2018/05/01 12:06:23 by jjourne          ###   ########.fr       */
+/*   Updated: 2018/05/01 17:56:22 by jjourne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,3 +17,19 @@ int		main(void)
 	printf("test\n");
 	return (0);
 }
+
+//------------------------------------------------------------------------------
+//sauvegarder
+	//rooms start / end (externe avec ptr sur room) (en interne, plusieurs type de room)
+	//les chemins
+	//parcourt profondeur/largeur, trouver un chemin, balancer
+//-----main.c
+
+//Main
+	// boucle get_next_line
+		//parsing
+	//le solveur
+
+// TIPS
+	// ne pas laisser les nom de salle avoir des tirets
+	//interpretation: plusieurs start(error, le dernier?)
